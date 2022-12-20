cc089b165b01cdfce78f397e6a0afaa7df47a9cb
Amin Mustafa Boankod <amin.m.boankod@gmail.com>
2022-12-20 16:29:42 +0100

    Change(test_yahtzee): add test for Yatzy.yahtzee

--- src/yahtzee.py
+++ src/yahtzee.py
@@ -8,16 +8,10 @@ class Yatzy:
 
     @staticmethod
     def chance(d1, d2, d3, d4, d5):
-        total = 0
-        total += d1
-        total += d2
-        total += d3
-        total += d4
-        total += d5
-        return total
+        return d1 + d2 + d3 + d4 + d5
 
     @staticmethod
-    def yatzy(dice):
+    def yahtzee(dice):
         counts = [0]*(len(dice)+1)
         for die in dice:
             counts[die-1] += 1
--- test/test_yahtzee.py
+++ test/test_yahtzee.py
@@ -1,3 +1,29 @@
 '''
 En este fichero escribiré los casos test que se vayan planteando posteriores a la lectura y comprensión del juego y las funciones de cada rutina.
 '''
+import pytest
+from src.yahtzee import Yatzy
+
+# Chance
+# The player scores the sum of all dice, no matter what they read.
+
+@pytest.mark.chance
+def test_chance():
+    # iterar sobre *args evita codigo cableado a 5 argumentos
+    assert 15 == Yatzy.chance(1, 2, 3, 4, 5)
+    assert 14 == Yatzy.chance(1, 1, 3, 3, 6)
+    assert 21 == Yatzy.chance(4, 5, 5, 6, 1)
+
+@pytest.mark.yahtzee_bonus
+def test_yahtzee():
+    bonus = [1,1,1,1,1]
+    assert 50 == Yatzy.yahtzee(bonus)
+    bonus = [6,6,6,6,6]
+    assert 50 == Yatzy.yahtzee(bonus)
+
+@pytest.mark.yahtzee_nonbonus
+def test_failyahtzee():
+    nonbonus = [1,1,1,2,1]
+    assert 0 == Yatzy.yahtzee(nonbonus)
+    nonbonus = [3,1,1,2,1]
+    assert 0 == Yatzy.yahtzee(nonbonus)
